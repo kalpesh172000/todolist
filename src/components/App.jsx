3bf0e74fac100e3db6ee28cf3930e343ff2312d9
kalpesh <kalpesh200071@gmail.com>
2024-06-21 09:06:15 +0530

    added todo app local

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import List from "./List";
+
+function App() {
+  return (
+    <List />
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+function List() {
+    const [inputText, setInputText] = useState(""); // Create a state variable to store the input text
+    const [items, setItems] = useState([]);
+
+    function addItems(event){
+        event.preventDefault();
+        const newItem = event.target.elements.itemInput.value;
+        setItems(oldItems =>[ ...oldItems, <li>{newItem}</li>])
+        setInputText("");
+    }
+
+    function handleChange(event){
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    return (
+        <form onSubmit={addItems} className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+                <input type="text" onChange={handleChange} name='itemInput' value={inputText}/>
+                <button type='submit'>
+                    <span>Add</span>
+                </button>
+            </div>
+            <div>
+                <ul>
+                    {items}
+                </ul>
+            </div>
+        </form>
+    );
+}
+
+export default List;
